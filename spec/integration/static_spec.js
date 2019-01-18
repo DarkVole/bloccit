5a0e1653848e0c8646dbd6d6210b30fbd00b13e8
Kevin Kohls <kevin.kohls@yahoo.com>
2019-01-17 22:01:41 -0500

    Assignment Answer for Express - add TDD for polo response

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,14 +5,15 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
 //#1
-  describe("GET /", () => {
+  describe("GET /macro", () => {
 
 //#2
     it("should return status code 200", (done) => {
 
 //#3
-      request.get(base, (err, res, body) => {
+       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(res.body.includes("polo")).toBe(true);
 
 //#4
         done();
