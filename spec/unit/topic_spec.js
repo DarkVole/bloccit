e472618087918785f5891ad112f7a95c92464549
Kevin Kohls <kevin.kohls@yahoo.com>
2019-02-03 14:39:52 -0500

    Testing works

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,48 +1,29 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
 
-describe("Post", () => {
+   describe("Post", () => {
 
   beforeEach((done) => {
 //#1
     this.topic;
     this.post;
-    sequelize.sync({force: true}).then((res) => {
+    //sequelize.sync({force: true}).then((res) => {
 
-//#2
+
+    //});
+
+  });
+});
+
+   describe("createTopics()", () => {
+     it("should validate a topic has been created", (done) => {
       Topic.create({
         title: "Expeditions Michigan",
         description: "A tour of Michigan."
       })
       .then((topic) => {
         this.topic = topic;
-//#3
-        Post.create({
-          title: "My first visit to Detroit",
-          body: "I saw some trees.",
-//#4
-          topicId: this.topic.id
-        })
-        .then((post) => {
-          this.post = post;
-          done();
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        done();
-      });
-    });
-
-  });
-});
-
-   describe("createTopics()", () => {
-
-     it("should validate a topic has been create", (done) => {
-
-       request.topic()
-       .then((topic) => {
          expect(topic.title).toBe("Expeditions Michigan");
          done();
        });
@@ -53,10 +34,16 @@ describe("Post", () => {
 
      it("should return the associated Post", (done) => {
 
-       this.topic.post()
-       .then((associatedPosts) => {
-         expect(associatedPosts.title).toBe("My first visit to Detroit");
+        Post.create({
+          title: "My first visit to Detroit",
+          body: "I saw some trees.",
+        topicId: this.topic.id
+                })
+
+        .then((posts) => {
+         expect(posts.title).toBe("My first visit to Detroit");
          done();
+         
        });
      });
    });  
