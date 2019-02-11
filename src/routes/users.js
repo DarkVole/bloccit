82c9e594e8db7649147ca167cb02870a7aff8353
Kevin Kohls <kevin.kohls@yahoo.com>
2019-02-11 13:19:19 -0500

    sign in npm fixed again

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const validation = require("./validation");
 
 const userController = require("../controllers/userController")
-router.post("/users/sign_up", validation.validateUsers, userController.create);
+router.post("/users/", validation.validateUsers, userController.create);
 router.get("/users/sign_in", userController.signInForm);
 router.get("/users/sign_out", userController.signOut);
 router.post("/users/sign_in", validation.validateUsers, userController.signIn);
