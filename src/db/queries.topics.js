6c636dadb981cf9347a0d45da392786f43cd3403
Kevin Kohls <kevinkohls1959@gmail.com>
2019-02-13 22:18:11 -0500

    First commit after checkpoint instructions

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -3,85 +3,123 @@ const Post = require("./models").Post;
 
 module.exports = {
 
-//#1
-  getAllTopics(callback){
-    return Topic.all()
+    //#1
+    getAllTopics(callback) {
+        return Topic.all()
 
 
-    .then((topics) => {
-      callback(null, topics);
-    })
-    .catch((err) => {
-      callback(err);
-    })
-  },
-  addTopic(newTopic, callback){
-      return Topic.create({
-        title: newTopic.title,
-        description: newTopic.description
-      })
-      .then((topic) => {
-        callback(null, topic);
-      })
-      .catch((err) => {
-        callback(err);
-      })
+            .then((topics) => {
+                callback(null, topics);
+            })
+            .catch((err) => {
+                callback(err);
+            })
     },
-  getTopic(id, callback){
-    return Topic.findById(id, {
 
-      include: [{
-        model: Post,
-        as: "posts"
+    addTopic(newTopic, callback) {
+        return Topic.create({
+                title: newTopic.title,
+                description: newTopic.description
+            })
+            .then((topic) => {
+                callback(null, topic);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+    },
+
+    getTopic(id, callback) {
+        return Topic.findById(id, {
+
+                include: [{
+                    model: Post,
+                    as: "posts"
       }]
-    })      
-      
-     .then((topic) => {
-       callback(null, topic);
-     })
-     .catch((err) => {
-       callback(err);
-     })
-   },
-  deleteTopic(id, callback){
-     return Topic.destroy({
-       where: {id}
-     })
-     .then((topic) => {
-       callback(null, topic);
-     })
-     .catch((err) => {
-       callback(err);
-     })
-   },
-  updateTopic(id, updatedTopic, callback){
-     return Topic.findById(id)
-     .then((topic) => {
-       if(!topic){
-         return callback("Topic not found");
-       }
+            })
 
-//#1
-       topic.update(updatedTopic, {
-         fields: Object.keys(updatedTopic)
-       })
-       .then(() => {
-         callback(null, topic);
-       })
-       .catch((err) => {
-         callback(err);
-       });
-     });
-   },
-  deletePost(id, callback){
-     return Post.destroy({
-       where: { id }
-     })
-     .then((deletedRecordsCount) => {
-       callback(null, deletedRecordsCount);
-     })
-     .catch((err) => {
-       callback(err);
-     })
-   }    
+            .then((topic) => {
+                callback(null, topic);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+    },
+
+    deleteTopic(req, callback) {
+
+        // #1
+        return Topic.findById(req.params.id)
+            .then((topic) => {
+
+                // #2
+                const authorized = new Authorizer(req.user, topic).destroy();
+
+                if (authorized) {
+                    // #3
+                    topic.destroy()
+                        .then((res) => {
+                            callback(null, topic);
+                        });
+
+                } else {
+
+                    // #4
+                    req.flash("notice", "You are not authorized to do that.")
+                    callback(401);
+                }
+            })
+            .catch((err) => {
+                callback(err);
+            });
+    },
+
+    updateTopic(req, updatedTopic, callback) {
+
+        // #1
+        return Topic.findById(req.params.id)
+            .then((topic) => {
+
+                // #2
+                if (!topic) {
+                    return callback("Topic not found");
+                }
+
+                // #3
+                const authorized = new Authorizer(req.user, topic).update();
+
+                if (authorized) {
+
+                    // #4
+                    topic.update(updatedTopic, {
+                            fields: Object.keys(updatedTopic)
+                        })
+                        .then(() => {
+                            callback(null, topic);
+                        })
+                        .catch((err) => {
+                            callback(err);
+                        });
+                } else {
+
+                    // #5
+                    req.flash("notice", "You are not authorized to do that.");
+                    callback("Forbidden");
+                }
+            });
+    },
+
+    deletePost(id, callback) {
+        return Post.destroy({
+                where: {
+                    id
+                }
+            })
+            .then((deletedRecordsCount) => {
+                callback(null, deletedRecordsCount);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+    }
 }
